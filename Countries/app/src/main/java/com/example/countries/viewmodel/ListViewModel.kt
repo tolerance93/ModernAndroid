95ebe207924693d6e69a8083bd20f42a016f2293
zpgy77@gmail.com <zpgy77@gmail.com>
2021-03-23 22:47:33 +0900

    ListViewModel Skeleton

--- Countries/app/src/main/java/com/example/countries/viewmodel/ListViewModel.kt
+++ Countries/app/src/main/java/com/example/countries/viewmodel/ListViewModel.kt
@@ -1,7 +1,33 @@
 package com.example.countries.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.countries.model.Country
 
 class ListViewModel: ViewModel() {
+    val countries = MutableLiveData<List<Country>>()
+    val countryLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
 
+    fun refresh() {
+        fetchCountries()
+    }
+
+    //Do not want expose
+    private fun fetchCountries() {
+        val mockData = listOf(Country("CountryA"),
+            Country("CountryB"),
+            Country("CountryC"),
+            Country("CountryD"),
+            Country("CountryE"),
+            Country("CountryF"),
+            Country("CountryG"),
+            Country("CountryH"),
+            Country("CountryI")
+        )
+
+        countryLoadError.value = false
+        loading.value = false
+        countries.value = mockData
+    }
 }
