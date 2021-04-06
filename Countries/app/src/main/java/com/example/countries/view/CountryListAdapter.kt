280e302da5a1fca8bf9e92b286e05ea9da3bad8d
zpgy77@gmail.com <zpgy77@gmail.com>
2021-04-06 21:36:20 +0900

    Load Image to ImageView

--- Countries/app/src/main/java/com/example/countries/view/CountryListAdapter.kt
+++ Countries/app/src/main/java/com/example/countries/view/CountryListAdapter.kt
@@ -6,6 +6,8 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.countries.R
 import com.example.countries.model.Country
+import com.example.countries.util.getProgressDrawable
+import com.example.countries.util.loadImage
 import kotlinx.android.synthetic.main.item_country.view.*
 
 class CountryListAdapter(val countries: ArrayList<Country>) : RecyclerView.Adapter<CountryListAdapter.CountryViewHolder>() {
@@ -28,10 +30,15 @@ class CountryListAdapter(val countries: ArrayList<Country>) : RecyclerView.Adapt
 
     class CountryViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
+        private val imageView = view.imageView
         private val countryName = view.name
+        private val countryCapital = view.capital
+        private val progressDrawable = getProgressDrawable(view.context)
 
         fun bind(country: Country) {
             countryName.text = country.countryName
+            countryCapital.text = country.capital
+            imageView.loadImage(country.flag, progressDrawable)
         }
     }
 }
