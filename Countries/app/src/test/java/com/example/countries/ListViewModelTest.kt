b23a24bd45f4bd69c3785799551ca4877f4d1050
zpgy77@gmail.com <zpgy77@gmail.com>
2021-04-19 21:27:00 +0900

    Test the success path

--- Countries/app/src/test/java/com/example/countries/ListViewModelTest.kt
+++ Countries/app/src/test/java/com/example/countries/ListViewModelTest.kt
@@ -1,19 +1,61 @@
 package com.example.countries
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.countries.model.CountriesService
+import com.example.countries.model.Country
+import com.example.countries.viewmodel.ListViewModel
 import io.reactivex.Scheduler
+import io.reactivex.Single
 import io.reactivex.android.plugins.RxAndroidPlugins
 import io.reactivex.disposables.Disposable
 import io.reactivex.internal.schedulers.ExecutorScheduler
 import io.reactivex.plugins.RxJavaPlugins
+import junit.framework.Assert.assertEquals
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
 import java.util.concurrent.Executor
 import java.util.concurrent.TimeUnit
 
 class ListViewModelTest {
     @get:Rule
     var rule = InstantTaskExecutorRule()
+
+    @Mock
+    lateinit var countriesService: CountriesService
+
+    @InjectMocks
+    var listViewModel = ListViewModel()
+
+    private var testSingle: Single<List<Country>>? = null
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+    }
+
+    @Test
+    fun getCountriesSuccess() {
+        val country = Country("countryName", "capital", "url")
+        val countriesList = arrayListOf(country)
+
+        testSingle = Single.just(countriesList)
+
+        `when`(countriesService.getCountries()).thenReturn(testSingle)
+
+        listViewModel.refresh()
+
+        Assert.assertEquals(1, listViewModel.countries.value?.size)
+        Assert.assertEquals(false, listViewModel.countryLoadError.value)
+        Assert.assertEquals(false, listViewModel.loading.value)
+    }
+
 
     @Before
     fun setUpRxSchedulers() {
